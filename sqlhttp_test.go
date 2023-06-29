18c3e8c6dec8a30336020bab798bc256d43ee495
meifakun <runner.mei@gmail.com>
2023-06-30 00:37:05 +0800

    add tests

--- sqlhttp_test.go
+++ sqlhttp_test.go
@@ -41,3 +41,39 @@ func TestSqlHttp(t *testing.T) {
 	runTest(t, target)
 	runTest(t, target)
 }
+
+func TestSqlHttpOpen(t *testing.T) {
+	dbname := os.Getenv("sqlhttp_db_name")
+	dbusername := os.Getenv("sqlhttp_db_username")
+	dbpassword := os.Getenv("sqlhttp_db_password")
+
+	fmt.Println(dbname, dbusername, dbpassword)
+
+	target, _, err := Open("db+http://localhost:8083/aceql?sc_dbname="+dbname+"&sc_dbtable=abc",
+		dbusername, dbpassword)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	sess, err := target.(*sqlhttpTarget).GetSession()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	_, err = target.(*sqlhttpTarget).c.ExecuteUpdate(sess, DefaultResetSQL, nil, false)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	_, err = target.(*sqlhttpTarget).c.ExecuteUpdate(sess, DefaultInitSQL, nil, false)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	runTest(t, target)
+	runTest(t, target)
+}
